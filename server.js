75133815cd84721e5412c3f997cea75b6b017c09
Taylor <tcuneo5@gmail.com>
2019-01-15 22:52:52 -0500

    server.js change

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require("express");
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
 var db = require('./models/index.js');
-require('./config/passport');
 
 console.log(db.User);
 db.User.findAll().then(users => {
@@ -13,8 +12,6 @@ db.User.findAll().then(users => {
 var PORT = process.env.PORT || 8080;
 
 var app = express();
-
-app.use(require('./routes'));
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
