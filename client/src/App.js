e75585d195712b489f87ebf187420a649b5a0530
Taylor <tcuneo5@gmail.com>
2019-01-19 14:22:18 -0500

    initial user routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Games from "./pages/Games";
+import Signup from "./pages/Signup";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Games} />
           <Route exact path="/games" component={Games} />
+          <Route exact path="/signup" component={Signup} />
           <Route component={NoMatch} />
         </Switch>
       </div>
--- server.js
+++ server.js
@@ -2,16 +2,24 @@ var express = require("express");
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
 var db = require('./models/index.js');
+const routes = require("./routes");
 
-console.log(db.User);
-db.User.findAll().then(users => {
-  console.log(users)
-});
-
-
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3001;
 
 var app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+// app.use(express.json());
+
+// Add routes, both API and view
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
