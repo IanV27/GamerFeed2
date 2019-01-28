27681cb0e4af0d75d07bdfadea6f2b969cc0d096
vpaumen <vpaumen@gmail.com>
2019-01-28 10:02:31 -0500

    fixed cursor to pointer text size

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
-        GamerFeed
+        Either remove this or make it the navigation with a button that says Register.
       </a>
     </nav>
   );
