3e72799324367af6f50ddb4d5cf4fd5c96f8d925
Taylor <tcuneo5@gmail.com>
2019-01-29 12:27:39 -0500

    mysql connection

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+var connection;
+
+if ( process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "password",
+      database: "gamerfeed_db"
+    });
+  }
+  
+  connection.connect();
+  module.exports = connection;
