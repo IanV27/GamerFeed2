2339728128b310a56fb1dd90139a3d3894a220f5
Taylor <tcuneo5@gmail.com>
2019-01-29 18:16:36 -0500

    heroku changes

---
+++ client/README.md
@@ -0,0 +1,7 @@
+# GamerFeed
+Find descriptions, videos and ratings for all your favorite games!
+
+## 
+
+Run Yarn Install and Npm install to download all packages
+Run Yarn start to run application on local computer.
--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
-var mysql = require('mysql');
-var connection;
+// var mysql = require('mysql');
+// var connection;
 
-if ( process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-  } else {
-    connection = mysql.createConnection({
-      host: "localhost",
-      port: 3306,
-      user: "root",
-      password: "NewJ@b$Prgrm18",
-      database: "gamerfeed_db"
-    });
-  }
+// if ( process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+//   } else {
+//     connection = mysql.createConnection({
+//       host: "localhost",
+//       port: 3306,
+//       user: "root",
+//       password: "NewJ@b$Prgrm18",
+//       database: "gamerfeed_db"
+//     });
+//   }
   
-  connection.connect();
-  module.exports = connection;
+//   connection.connect();
+//   module.exports = connection;
